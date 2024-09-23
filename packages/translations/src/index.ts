fe5df1b3c464569a18b6e155183a9210f5a5bbd7
drepkovsky <dominik.repkovsky@gmail.com>
2024-09-23 23:22:32 +0200

    feat: add translations setup

--- packages/translations/src/index.ts
+++ packages/translations/src/index.ts
@@ -1,7 +1,9 @@
-import en from './static/en.json'
+import de from './i18n/de/translations.json'
+import en from './i18n/en/translations.json'
 
 export const allTranslations = {
   en,
+  de,
 }
 
 export const SUPPORTED_LOCALES = Object.keys(allTranslations) as SupportedLocale[]
--- packages/translations/src/translation-types.ts
+++ packages/translations/src/translation-types.ts
@@ -1,5 +1,6 @@
 import type { LeafPaths } from '@bulkit/shared/types/data'
+import type { allTranslations } from '@bulkit/translations/index'
 
-export type TranslationsRaw = typeof import('./static/en.json')
+export type TranslationsRaw = keyof (typeof allTranslations)['en']
 
 export type TranslationKey = LeafPaths<TranslationsRaw>
