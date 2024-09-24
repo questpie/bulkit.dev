2a22ecda34ffbf4077066441018713d283348a75
drepkovsky <dominik.repkovsky@gmail.com>
2024-09-24 16:37:24 +0200

    chore: remove accent

--- packages/translations/src/index.ts
+++ packages/translations/src/index.ts
@@ -1,9 +1,7 @@
-import de from './i18n/de/translations.json'
 import en from './i18n/en/translations.json'
 
 export const allTranslations = {
   en,
-  de,
 }
 
 export const SUPPORTED_LOCALES = Object.keys(allTranslations) as SupportedLocale[]
