56f33fafa8046b345ede940766a6ac7ae2e6e715
drepkovsky <dominik.repkovsky@gmail.com>
2024-11-18 00:49:57 +0100

    fix: design

--- apps/app/src/app/(auth)/login/layout.tsx
+++ apps/app/src/app/(auth)/login/layout.tsx
@@ -2,8 +2,6 @@ import type { PropsWithChildren } from 'react'
 
 export default function AuthLayout(props: PropsWithChildren) {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
-      {props.children}
-    </main>
+    <main className='flex min-h-screen flex-col items-center justify-center'>{props.children}</main>
   )
 }
