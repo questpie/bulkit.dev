23f6025a6fa5b55800dba2f6b347892f68e45f5a
drepkovsky <dominik.repkovsky@gmail.com>
2024-11-28 01:34:42 +0100

    fix: config

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -1,5 +1,6 @@
-import { command, positional, run } from '@drizzle-team/brocli'
+import { boolean, command, positional, run } from '@drizzle-team/brocli'
 import { createPackage } from '@bulkit/script/commands/create-package'
+import { release } from '@bulkit/script/commands/release'
 
 const createPackageCommand = command({
   name: 'create:package',
@@ -15,4 +16,15 @@ const createPackageCommand = command({
   },
 })
 
-run([createPackageCommand]) // parse shell arguments and run command
+const releaseCommand = command({
+  name: 'release',
+  desc: 'Release a new version of the packages',
+  options: {
+    next: boolean().desc('Release a next version').default(false),
+  },
+  handler: async (opts) => {
+    return await release({ next: opts.next })
+  },
+})
+
+run([createPackageCommand, releaseCommand]) // parse shell arguments and run command
