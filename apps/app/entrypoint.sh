8c1f6bc82f5a0e4d08be8b4f7c1747e978ad4d64
drepkovsky <dominik.repkovsky@gmail.com>
2024-12-02 00:40:06 +0100

    fix: app entrypoint

--- apps/app/entrypoint.sh
+++ apps/app/entrypoint.sh
@@ -6,7 +6,7 @@ printenv | grep NEXT_PUBLIC_ | while read -r line ; do
   key=$(echo $line | cut -d "=" -f1)
   value=$(echo $line | cut -d "=" -f2)
 
-  find /usr/src/app/.next/ -type f -exec sed -i "s|$key|$value|g" {} \;
+  find /usr/src/app/apps/app/.next/ -type f -exec sed -i "s|$key|$value|g" {} \;
 done
 
 # Execute the container's main process (CMD in Dockerfile)
