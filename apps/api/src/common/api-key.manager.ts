3b9d807e46e5d1826cceb87d14515f020c4d9f2c
drepkovsky <dominik.repkovsky@gmail.com>
2024-12-05 18:52:35 +0100

    chore: remove console.logs

--- apps/api/src/common/api-key.manager.ts
+++ apps/api/src/common/api-key.manager.ts
@@ -46,8 +46,6 @@ export class ApiKeyManager {
       })
     }
 
-    console.log('encryptedApiKey', encryptedApiKey, this.#ENCRYPTION_KEY, envApi.ENCRYPTION_SECRET)
-
     try {
       const [ivHex, encryptedHex] = encryptedApiKey.split(':')
       const iv = Buffer.from(ivHex!, 'hex')
